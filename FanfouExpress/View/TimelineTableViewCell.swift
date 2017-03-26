af01ff7f972f693168df70f652c1b931b847fcf3
zchan0 <zchan0@outlook.com>
2017-03-26 16:47:44 -0400

    Add TimelineTableViewCell

---
+++ FanfouExpress/View/TimelineTableViewCell.swift
@@ -0,0 +1,107 @@
+//
+//  TimelineTableViewCell.swift
+//  FanfouExpress
+//
+//  Created by Cencen Zheng on 3/25/17.
+//  Copyright © 2017 Cencen Zheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    private var contentLabel: UILabel
+    private var screenNameLabel: UILabel
+    private var previewImageView: UIImageView
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        self.contentLabel = UILabel()
+        self.screenNameLabel = UILabel()
+        self.previewImageView = UIImageView()
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentLabel.numberOfLines = 0
+        self.contentLabel.font = CellStyle.ContentFont
+        
+        self.screenNameLabel.textAlignment = .right
+        self.screenNameLabel.font = CellStyle.ScrrenNameFont
+        
+        self.previewImageView.isHidden = true
+        self.previewImageView.clipsToBounds = true
+        self.previewImageView.contentMode = .scaleAspectFill
+        self.previewImageView.backgroundColor = UIColor.lightGray
+        
+        self.contentView.addSubview(contentLabel)
+        self.contentView.addSubview(screenNameLabel)
+        self.contentView.addSubview(previewImageView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        let contentRect = UIEdgeInsetsInsetRect(CGRect(x: 0, y: 0, width: contentView.bounds.width, height: contentView.bounds.height), CellStyle.ContentInsets)
+        let contentSize = contentRect.size
+        let contentLabelSize = contentLabel.sizeThatFits(contentSize)
+        let screenNameSize   = screenNameLabel.sizeThatFits(contentSize)
+
+        return CGSize(width: size.width,
+                      height: CellStyle.ContentInsets.top
+                        + contentLabelSize.height + CellStyle.ContentVerticalMargin
+                        + (previewImageView.isHidden ? 0 : CellStyle.ImageHeight + CellStyle.PreviewVerticalMargin)
+                        + screenNameSize.height
+                        + CellStyle.ContentInsets.bottom)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let contentSize = CGSize(width: contentView.bounds.width - CellStyle.ContentInsets.left - CellStyle.ContentInsets.right,
+                                 height: contentView.bounds.height - CellStyle.ContentInsets.top - CellStyle.ContentInsets.bottom)
+        let contentWidth = contentSize.width
+        
+        contentLabel.frame = {
+            let size = contentLabel.sizeThatFits(contentSize)
+            let frame = CGRect(origin: CGPoint(x: CellStyle.ContentInsets.left, y: CellStyle.ContentInsets.top),
+                               size: CGSize(width: contentWidth, height: size.height))
+            return frame
+        }()
+        
+        if previewImageView.isHidden == false {
+            previewImageView.frame = {
+                return CGRect(origin: CGPoint(x: CellStyle.ContentInsets.left, y: contentLabel.frame.maxY + CellStyle.ContentVerticalMargin),
+                                   size: CGSize(width: contentWidth, height: CellStyle.ImageHeight))
+            }()
+        }
+        
+        screenNameLabel.frame = {
+            let size = screenNameLabel.sizeThatFits(contentSize)
+            let frame = CGRect(origin: CGPoint(x: CellStyle.ContentInsets.left,
+                                               y: previewImageView.isHidden ? contentLabel.frame.maxY + CellStyle.ContentVerticalMargin : previewImageView.frame.maxY + CellStyle.PreviewVerticalMargin),
+                               size: CGSize(width: contentWidth, height: size.height))
+            return frame
+        }()
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        contentLabel.text = nil
+        screenNameLabel.text = nil
+        previewImageView.image = nil
+        previewImageView.isHidden = true
+    }
+    
+    func updateContentWith(_ msg: Message) {
+        contentLabel.text = msg.content
+        screenNameLabel.text = "—— \(msg.realName)"
+        
+        if let imageURL = msg.image?.previewURL {
+            previewImageView.isHidden = false
+            previewImageView.setImage(withURL: imageURL)
+        }
+    }
+}
+
